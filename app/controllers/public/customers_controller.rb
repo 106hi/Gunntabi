39032cc5bbe4d41c94c5d3bb5f943b75861b225e
106hi <hiro3428shs96kzsi@gmail.com>
2023-06-11 11:03:18 +0000

    Add customer's edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,10 +5,24 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
 
+  def update
+    customer = Customer.find(params[:id])
+    if customer.update(customer_params)
+      redirect_to customer_path(customer.id)
+    else
+      render :edit
+    end
   end
 
   def confirm
 
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(%i[first_name last_name nickname phone_number email profile])
   end
 end
