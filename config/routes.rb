118ddbba4c7f07c9485736a60ca5a680d5ded723
106hi <hiro3428shs96kzsi@gmail.com>
2023-06-12 11:56:54 +0000

    Add log_in function for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'homes/top'
+  end
   devise_for :admin, skip: %i[registrations passwords], controllers: {
     sessions: "admin/sessions"
   }
@@ -11,6 +14,10 @@ Rails.application.routes.draw do
     root to: "homes#top"
     resources :posts, only: %i[new index show edit create destroy update]
     resources :customers, only: %i[show edit update]
+  end
+
+  namespace :admin do
+    get "/" => "homes#top"
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
