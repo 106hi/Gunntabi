d2c37d092e31d048c7c4708f9e7cf4cd9699d6e1
106hi <hiro3428shs96kzsi@gmail.com>
2023-06-14 20:24:30 +0000

    Add comment delete function

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,20 +2,23 @@ class Public::PostCommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    comment = current_customer.post_comments.new(post_comment_params)
-    comment.post_id = @post.id
-    comment.save
+    @comment = current_customer.post_comments.new(post_comment_params)
+    @comment.post_id = @post.id
+    @comment.save
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
-    @post_comments = @post.coments
-    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    @post_comment = PostComment.find(params[:id])
+    @post_comment.destroy
+    # @post = Post.find(params[:post_id])
+    # @post_comments = @post.post_comments
+    # @post_comments.destroy
+    # PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
   end
 
   private
 
-  def post_coment_params
+  def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
 end
