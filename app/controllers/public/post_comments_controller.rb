5fc743c3ec17f2abf4d9322a560f72284aa6f15b
106hi <hiro3428shs96kzsi@gmail.com>
2023-06-15 03:34:55 +0000

    Fix variable to post_comments_controller

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,19 +2,15 @@ class Public::PostCommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = current_customer.post_comments.new(post_comment_params)
-    @comment.post_id = @post.id
-    @comment.save
+    comment = current_customer.post_comments.new(post_comment_params)
+    comment.post_id = @post.id
+    comment.save
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    @post_comment = PostComment.find(params[:id])
-    @post_comment.destroy
-    # @post = Post.find(params[:post_id])
-    # @post_comments = @post.post_comments
-    # @post_comments.destroy
-    # PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    post_comment = PostComment.find(params[:id])
+    post_comment.destroy
   end
 
   private
