69c7bf005facf0a2058f5e583c08d4e2685ae4fb
106hi <hiro3428shs96kzsi@gmail.com>
2023-06-18 17:59:06 +0000

    Fix layout admin

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,9 +8,23 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
+  def update
+    customer = Customer.find(params[:id])
+    if customer.update(customer_params)
+      redirect_to admin_customer_path(customer.id)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @customer = Customer.find(params[:id])
     @customer.destroy
     redirect_to '/admin'
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(%i[first_name last_name nickname phone_number email])
   end
 end
