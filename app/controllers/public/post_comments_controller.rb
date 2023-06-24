2197da249d710c63493fa84d97bb6cb7bbfe2fe0
106hi <hiro3428shs96kzsi@gmail.com>
2023-06-24 13:41:32 +0000

    Fix layout

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -5,12 +5,14 @@ class Public::PostCommentsController < ApplicationController
     @comment = current_customer.post_comments.new(post_comment_params)
     @comment.post_id = @post.id
     if @comment.save
-      #flash.now[:notice] = 'コメントを投稿しました'
+      flash.now[:notice] = 'コメントを投稿しました'
+      render :create
       # redirect_to post_path(@post)
       #redirect_to post_path(@post), notice: 'コメントを投稿しました'
     else
-      flash[:alert] = "コメントを入力してください."
-      redirect_to post_path(@post.id)
+      # flash[:alert] = "コメントを入力してください"
+      # redirect_to post_path(@post.id)
+      render :error
     end
   end
 
@@ -18,6 +20,10 @@ class Public::PostCommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     post_comment = PostComment.find(params[:id])
     post_comment.destroy
+    flash.now[:alert] = '投稿を削除しました'
+    #renderしたときに@postのデータがないので@postを定義
+    # @post = Post.find(params[:post_id])
+    render :destroy  #render先にjsファイルを指定
   end
 
   private
